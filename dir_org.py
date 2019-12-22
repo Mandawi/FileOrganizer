399c6732cfb2e4fc8a3d85ccf074d486056c64cf
Osamah Mandawi <oamandawi@brandeis.edu>
2019-12-21 22:32:03 -0500

    Update dir_org.py

--- dir_org.py
+++ dir_org.py
@@ -1,8 +1,25 @@
 import os
+import shutil
 
+types_list=[]
+files_list=[]
 types=set()
 
 for file in os.listdir():
-    types.add(file[:file.index(".")])
+    if os.path.isfile(file):
+        typ=file[file.rfind(".")+1:].upper()
+        types_list.append(typ)
+        if types_list.count(typ)>1:
+            types.add(typ)
+
+for folder in types:
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+for file in os.listdir():
+    typ=file[file.rfind(".")+1:].upper()
+    if typ in types:
+        os.rename(file, f"typ/{file}")
+
 
 print(types)
